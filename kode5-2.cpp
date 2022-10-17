3c4f9dcbfd449bd2a1938756b1f7b7184ffa9269
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-10-17 13:56:55 +0700

    Add files via upload

---
+++ kode5-2.cpp
@@ -0,0 +1,33 @@
+/**********************************************************
+*  PEMROGRAMAN C++: MUDAH & CEPAT MENJADI MASTER C++      *
+*                                                         *
+*  Oleh    : Budi Raharjo                                 *
+*  Email   : mbraharjo@gmail.com                          *
+*                                                         *
+*  Website : www.best-informatic.com                      *
+*                                                         *
+***********************************************************/
+/*DI Tingkatkan kembali oleh
+/**********************************************************
+*  PEMROGRAMAN C++                                        *
+*                                                         *
+*  Author  : Gesa Rizky Nugraha                           *
+*  Email   : gesarizkynugraha@gmail.com                   *
+*                                                         *
+*  Website : karenabelajar.blogspotcom                    *
+*                                                         *
+***********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int C;
+  // pengulngan 10 kali
+  for (C=0; C<10; C++) {
+    cout<<"Bisa Karena Belajar"<<endl;
+  }
+
+  return 0;
+}
